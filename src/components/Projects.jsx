6b2ed1836d12648bc64cd17ee65fae466c5b286b
Konstantin <knetsov91@gmail.com>
2025-06-20 16:30:56 +0300

    Add Projects component that render Project components.

---
+++ src/components/Projects.jsx
@@ -0,0 +1,19 @@
+import data from "../data.json";
+import Project from "./Project";
+import { useState } from "react";
+
+const Projects = () => {
+    const [projects, setProjects] = useState(data); 
+
+    return (
+        <>
+            {projects.map(p => <Project
+                                    project={p}
+                                    key={p.id} />
+                        )
+            }
+        </>
+    );
+}
+
+export default Projects;

9920339df86a47010d36dc3a5653680c36aa3a32
Konstantin <knetsov91@gmail.com>
2025-06-20 20:09:18 +0300

    Add count of tags for every project in Project component.

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -6,6 +6,7 @@ const Project = ({project}) => {
             <h2>Social Network</h2>
             <p>{project.description}</p>
             <a href={project.link} target="_blank">Link</a>
+            <span>Tags({project.tags.length}):</span>
             <div className={style.tags}>
                 {
                     project.tags.map(( tag) => {

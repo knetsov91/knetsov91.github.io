7d07f8dcc8f7be26a8066a87fe8a2b9d4dfc4c64
Konstantin <knetsov91@gmail.com>
2025-06-20 16:28:21 +0300

    Add Projects component in App.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import { useState } from 'react'
 import './App.css' 
 import Projects from './components/Projects'
 
 function App() {
-
   return (
     <>
       <h1>Projects</h1>
       <div className="main">
+        <Projects />
       </div>
     </>
   )

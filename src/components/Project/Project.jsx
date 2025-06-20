0ecbe58cc913fc13769f0448da2a01d7ff222d51
Konstantin <knetsov91@gmail.com>
2025-06-20 21:49:45 +0300

    Add bold tag for tags count in Project component.

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -6,7 +6,7 @@ const Project = ({project}) => {
             <h2>{project.name}</h2>
             <p>{project.description || <span>There is no description yet</span> }</p>
             <a href={project.link} target="_blank">Link</a>
-            <span>Tags({project.tags.length}):</span>
+            <span>Tags(<b>{project.tags.length}</b>):</span>
             <div className={style.tags}>
                 {
                     project.tags.map(( tag) => {

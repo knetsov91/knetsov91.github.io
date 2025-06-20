7d72b575b49bdfb0f2ddd1a01e795c256ac15900
Konstantin <knetsov91@gmail.com>
2025-06-20 16:58:05 +0300

    Load JSON data with useEffect hook when Projects component is loaded.

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,10 +1,14 @@
-import data from "../data.json";
-import Project from "./Project/Project";
+import { useEffect } from "react";
+import data from "../../data.json";
+import Project from "../Project/Project";
 import { useState } from "react";
 
 const Projects = () => {
-    const [projects, setProjects] = useState(data); 
+    const [projects, setProjects] = useState([]); 
 
+    useEffect(() => {
+        setProjects(data);
+    }, []);
     return (
         <>
             {projects.map(p => <Project

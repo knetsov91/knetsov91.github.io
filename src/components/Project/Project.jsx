430d57214bdc2c1ac53630cf13fb6b5ac311f42d
Konstantin <knetsov91@gmail.com>
2025-06-20 20:06:17 +0300

    Add class for tags section in Project component.

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -6,7 +6,7 @@ const Project = ({project}) => {
             <h2>Social Network</h2>
             <p>{project.description}</p>
             <a href={project.link} target="_blank">Link</a>
-            <div className="project-tags">
+            <div className={style.tags}>
                 {
                     project.tags.map(( tag) => {
                         return <span className={style.tag}

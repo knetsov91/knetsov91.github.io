41b5e297d5a3a0175ee69ce4e891822c837fdb46
Konstantin <knetsov91@gmail.com>
2025-10-29 22:33:23 +0200

    feat(Project): Render projects create and update dates.

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -1,9 +1,25 @@
+import { useEffect, useState } from 'react';
 import style from './Project.module.css';
 
+import { fetchRepoData, generateRepoUrl } from '../../utils/util';
+
 const Project = ({project}) => {
+    const [createdAt, setCreatedAt] = useState(null) 
+    const [updatedAt, setUpdatedAt] = useState(null) 
+    const fetchUrl = generateRepoUrl(project.link);
+ 
+    useEffect(() => {
+       fetchRepoData(fetchUrl)
+       .then((data) => { 
+            setCreatedAt(new Date(data.created_at).toLocaleDateString());
+            setUpdatedAt(new Date(data.updated_at).toLocaleDateString());
+       });
+    }, []);
     return (
         <div className={style.project}>
             <h2>{project.name}</h2>
+            <span>Created at: <b>{createdAt}</b></span>
+            <span>Last update: <b>{updatedAt}</b></span>
             <p>{project.description || <span>There is no description yet</span> }</p>
             <a href={project.link} target="_blank">Link</a>
             <span>Tags(<b>{project.tags.length}</b>):</span>

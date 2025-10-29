b60a108b91b35dc8004441411e07492bc6276463
Konstantin <knetsov91@gmail.com>
2025-10-29 22:29:24 +0200

    feat(api): Add function to fetch repository data

--- src/utils/util.js
+++ src/utils/util.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 const gitApiUrl = 'https://api.github.com/repos/knetsov91/';
 
 export const generateRepoUrl = (projectUrl) => {
@@ -5,4 +6,12 @@ export const generateRepoUrl = (projectUrl) => {
     const lastSlashIndex = projectUrl.lastIndexOf('/');
     const projectRepoName = projectUrl.slice(lastSlashIndex + 1, projectUrlLength);
     return gitApiUrl + projectRepoName;
+}
+
+export const fetchRepoData = (fetchUrl) => {
+  return axios.get(`${fetchUrl}`)
+        .then(( response ) => response.data)
+        .catch(err => {
+            console.log(err)
+        }) 
 }
